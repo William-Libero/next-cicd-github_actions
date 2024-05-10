34ad3644ac0f99528507c312f755604244596b8f
william <williamlibero@profkenny.com.br>
2024-05-10 09:42:04 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ export default function HomeScreen() {
   return (
     <div>
       <h1>Home</h1>
+      <p>Nova alteração</p>
     </div>
   );
 }
--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -4,5 +4,7 @@ import { encode } from "punycode";
 console.log("[DEPLOY_PREVIEW]: START");
 const command = "yarn deploy:staging";
 const output = execSync(command, { encoding: "utf-8" });
-console.log(output);
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length - 1];
 console.log("[DEPLOY_PREVIEW]: END");
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
