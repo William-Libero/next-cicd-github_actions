1b9082eb60009cee9a8fa750e65c40973ae6c53f
william <williamlibero@profkenny.com.br>
2024-05-10 09:36:35 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,8 @@
-console.log("olá mundo");
+import { execSync } from "child_process";
+import { encode } from "punycode";
+
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: END");
